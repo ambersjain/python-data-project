7cf13495882e1c385e45a8c1773bbb72b9740d0b
Amber Jain <amberjain1234@gmail.com>
2021-01-08 17:57:11 +1100

    Keep reading the file

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import os
 import csv
+import pandas
+
 
 def main():
-    try:
-        print("===================Load the CSV file===================")
-        filename = input("Type in the name of the file along with extension: ")
-        f = open(filename)
-        print("CSV file loaded")
-    except Exception as e:
-        # error loading file
-        print("The file cannot be loaded: ", e)
-        exit()
+    fileread = False
+    while not fileread:
+        try:
+            print("===================Load the CSV file===================")
+            filename = input("Type in the name of the file along with extension: ")
+            f = open(filename)
+            print("CSV file loaded")
+            fileread = True
+        except Exception as e:
+            # error loading file
+            print("The file cannot be loaded: ", e)
 
 if __name__ == "__main__":
     main()
