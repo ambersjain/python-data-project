7a6fdaf07d8e711d08a267bb7b94235e7be0a903
Amber Jain <amberjain1234@gmail.com>
2021-01-09 17:52:23 +1100

    Added exit option in the menu

--- main.py
+++ main.py
@@ -40,19 +40,20 @@ def main():
                 print("1. Convert CSV to JSON")
                 print("2. Check Data Summary")
                 print("3. Generate a SQL insert statement for all rows in the input")
+                print("4. Exit")
                 option = input("Select 1, 2 or 3 from above: ")
                 if option == '1':
-                    correct_option_selected = True
                     print("Converting CSV to JSON......")
                     convertToJson(data)
                 elif option == '2':
-                    correct_option_selected = True
                     print("Presenting data summary......")
                     presentDataSummary()
                 elif option == '3':
-                    correct_option_selected = True
                     print("Generate a SQL insert statement for all rows in the input......")
                     sqlInsert()
+                elif option == '4':
+                    correct_option_selected = True
+                    print("Thanks for using the app.")
                 else:
                     print("Please input a correct integer [1,2 or 3]......")
 
